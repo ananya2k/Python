8fd9ce9980052cbce9b930332cce0a688610eca8
ananya2k <ananya2k@gmail.com>
2020-09-18 11:08:23 -0400

    FizzBuzz program

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Hello world!!!")
+def fizzbuzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    if number % 3 == 0:
+        return "Fizz"
+    if number % 5 == 0:
+        return "Buzz"
+    return number
+
+
+num = int(input("Enter a number: "))
+print(fizzbuzz(num))
